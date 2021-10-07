41b9482aa456a574b6cec54a3c98163bf86e5942
Robin Douet <robin.douet@gmail.com>
2021-10-07 21:12:37 +0200

    Adding commentary
    The code is commented

--- Assets/Scripts/RoadToGoal.cs
+++ Assets/Scripts/RoadToGoal.cs
@@ -14,17 +14,17 @@ public class RoadToGoal : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent.destination = goal.position;  // Set the destination to the ennemy 
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if the distance between the ennemy and the goal is under 0.2, the agent is destroyed
+        // If the distance between the ennemy and the goal is under 0.2, the agent is destroyed
         if (agent.remainingDistance < 0.2)
         {
-            Destroy(gameObject);
+            Destroy(gameObject);  // Destroy the ennemy
             print(spawnerSous.GetComponent<Spawning>().compteur);
         }
     }
