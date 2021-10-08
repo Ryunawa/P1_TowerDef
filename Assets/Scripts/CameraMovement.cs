e090801ccf54e4fb6ab97ac8a09f512149dc8c60
Robin DOUET <robin.douet@gmail.com>
2021-10-08 09:41:54 +0200

    Camera Movement added
    We can move the camera with Z, Q, S, D, (or directives arrows), CTRL, SHIFT

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float speed = 9;
+    
+    GameObject character;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        { 
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+    }
+
+
+}
